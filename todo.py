604c6c6aab9273de936df94b79065367ed8a8825
Deandre Huang <deandre.huang14@gmail.com>
2025-07-24 21:55:16 -0700

    - add remove_task function

--- todo.py
+++ todo.py
@@ -1,19 +1,28 @@
 tasks = []
 
-def add_task(task):
-    tasks.append(task)
+# Adds a singular user-inputted task to the tasks list
+def add_task():
+    task_to_add = input("Enter a task to add: ")
+    tasks.append(task_to_add)
 
+# Prints every task in the tasks list
 def view_tasks():
     for task in tasks:
         print(task)
+
+# Remove a user-specified task
+def remove_task():
+    task_to_remove = input("Enter a task to remove: ")
+    tasks.remove(task_to_remove)
 
 def main():
     print("Add 3 tasks")
     counter = 0
     while counter < 3:
-        input_task = input("Enter a task: ")
-        add_task(input_task)
+        add_task()
         counter += 1
+    view_tasks()
+    remove_task()
     view_tasks()
 
 if __name__ == '__main__':
