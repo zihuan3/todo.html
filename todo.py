81e37dd58b115f18b3dd7c0430e9f52375d30f96
Deandre Huang <deandre.huang14@gmail.com>
2025-07-24 22:01:30 -0700

    - add view_completed_tasks - add mark_completed

--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
 tasks = []
+completed_tasks = []
 
 # Adds a singular user-inputted task to the tasks list
 def add_task():
@@ -7,13 +8,23 @@ def add_task():
 
 # Prints every task in the tasks list
 def view_tasks():
+    print("Tasks:")
     for task in tasks:
         print(task)
+
+def view_completed_tasks():
+    print("Completed tasks:")
+    for completed_task in completed_tasks:
+        print(completed_task)
 
 # Remove a user-specified task
 def remove_task():
     task_to_remove = input("Enter a task to remove: ")
     tasks.remove(task_to_remove)
+
+def mark_completed():
+    task_to_mark = input("Enter a task to mark completed: ")
+    completed_tasks.append(task_to_mark)
 
 def main():
     print("Add 3 tasks")
@@ -24,6 +35,9 @@ def main():
     view_tasks()
     remove_task()
     view_tasks()
+    mark_completed()
+    view_tasks()
+    view_completed_tasks()
 
 if __name__ == '__main__':
     main()
